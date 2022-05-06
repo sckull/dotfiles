34ae442c4236a1575c736aea553b62971fdf4287
sckull <danysc32@gmail.com>
2022-05-05 18:01:12 -0600

    a

--- .config/scripts/vpn-copy.sh
+++ .config/scripts/vpn-copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-vpn=$(ip addr show dev tun0 | grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | cut -d $'\n' -f1| awk '{print $1}')
+vpn=$(ip addr show dev $1 | grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | cut -d $'\n' -f1| awk '{print $1}')
 echo -n $vpn | xclip -sel clip
 notify-send -t 3000 -u normal "$vpn copiada" -i $HOME/.config/icons/online.svg
